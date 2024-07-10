412ccd6341f5248ec1c40c3bb6c6fe6728f54af3
Paktakraw <tresarpnat27@gmail.com>
2024-07-10 14:47:01 +0700

    solv Problem Solving 1

--- ex-1.js
+++ ex-1.js
@@ -1,3 +1,17 @@
 function twoSum(numbers, target) {
-	// เริ่มเขียนโค้ดตรงนี้จ้า
-};
+  // เริ่มเขียนโค้ดตรงนี้จ้า
+  for (let left = 0; (right = numbers.length - 1); left < right) {
+    const sum = numbers[left] + numbers[right];
+    if (sum === target) {
+      return [left, right];
+    } else if (sum < target) {
+      left++;
+    } else {
+      right--;
+    }
+  }
+  return [];
+}
+let numbers = [2, 7, 11, 15];
+let target = 9;
+console.log(twoSum(numbers, target));
