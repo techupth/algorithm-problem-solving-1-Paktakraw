a4741c9bf0b8fcc617c37f406720589c97700766
Paktakraw <tresarpnat27@gmail.com>
2024-07-15 17:28:12 +0700

    solv Algorithm: Problem Solving 1 complete

--- ex-1.js
+++ ex-1.js
@@ -1,7 +1,27 @@
 function twoSum(numbers, target) {
   // เริ่มเขียนโค้ดตรงนี้จ้า
-  for (let left = 0; (right = numbers.length - 1); left < right) {
-    const sum = numbers[left] + numbers[right];
+  // for (let left = 0; (right = numbers.length - 1); left < right) {
+  //   const sum = numbers[left] + numbers[right];
+  //   if (sum === target) {
+  //     return [left, right];
+  //   } else if (sum < target) {
+  //     left++;
+  //   } else {
+  //     right--;
+  //   }
+  // }
+  // return [];
+
+  //index
+  let left = 0;
+  let right = numbers.length - 1;
+
+  // console.log(numbers[left]);
+  // console.log([numbers[left], numbers[right]]);
+
+  while (left < right) {
+    //ค่าในindex
+    let sum = numbers[left] + numbers[right];
     if (sum === target) {
       return [left, right];
     } else if (sum < target) {
